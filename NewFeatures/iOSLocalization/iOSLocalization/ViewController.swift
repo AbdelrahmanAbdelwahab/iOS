5c904f5fd14c4f75fda06546ec752adb4c6554eb
Abdelwahab <abdelrahman.abdelwahab@mooney.it>
2021-12-24 17:58:13 +0100

    ARA TEXT

--- NewFeatures/iOSLocalization/iOSLocalization/ViewController.swift
+++ NewFeatures/iOSLocalization/iOSLocalization/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
+    var ara = "التفكير فيك يبقيني مستيقظًا. الحلم بك يبقيني نائما. التواجد معك يبقيني على قيد الحياة "
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        textLabel.text = ara
     }
 
 
