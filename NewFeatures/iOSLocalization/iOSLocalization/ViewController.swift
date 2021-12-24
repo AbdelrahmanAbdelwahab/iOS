a65046b98b4a769306ced6906e624fb995130a70
Abdelwahab <abdelrahman.abdelwahab@mooney.it>
2021-12-24 18:23:48 +0100

    add arabic Language support

--- NewFeatures/iOSLocalization/iOSLocalization/ViewController.swift
+++ NewFeatures/iOSLocalization/iOSLocalization/ViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
-    var ara = "التفكير فيك يبقيني مستيقظًا. الحلم بك يبقيني نائما. التواجد معك يبقيني على قيد الحياة "
     override func viewDidLoad() {
         super.viewDidLoad()
-//        textLabel.text = ara
+        textLabel.text = String(localized: "KEEP_MESS")        
     }
 
 
