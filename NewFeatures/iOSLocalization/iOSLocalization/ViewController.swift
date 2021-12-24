09d145857230e7af46f1f5415761746a41cec65a
Abdelwahab <abdelrahman.abdelwahab@mooney.it>
2021-12-24 13:22:55 +0100

    Localization

---
+++ NewFeatures/iOSLocalization/iOSLocalization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSLocalization
+//
+//  Created by ABDELWAHAB on 24/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
